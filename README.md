fbfde551c8d05891b78bfff16ca70139f5bbcae7
anjia0532 <anjia0532@gmail.com>
2017-08-16 16:08:19 +0800

    增加readme.md

---
+++ README.md
@@ -0,0 +1,195 @@
+
+## 介绍
+
+本项目是基于[openresty/lua-resty-redis][] 是[章亦春（agentzh）][agentzh]开发的openresty中的操作redis的库。进行二次封装的工具库。核心功能还是由[openresty/lua-resty-redis][]完成的。
+
+本文假设你已经了解nginx+lua或者openresty如何使用lua脚本(e.g. `lua_package_path`配置,`*_by_lua_file`),基础的redis相关知识，以及[openresty/lua-resty-redis][]的基本使用方法([openresty/lua-resty-redis#README.md][README.md])。
+
+## 对比
+
+截取官方部分代码，进行说明
+
+```lua
+    local redis = require "resty.redis"
+    local red = redis:new()
+
+    red:set_timeout(1000) -- 1 sec --设置超时时间
+
+    local ok, err = red:connect("127.0.0.1", 6379) --设置redis的host和port
+    if not ok then --判断生成连接是否失败
+        ngx.say("failed to connect: ", err)
+        return
+    end
+
+    ok, err = red:set("dog", "an animal") --插入键值(类似 mysql insert)
+    if not ok then --判断操作是否成功
+        ngx.say("failed to set dog: ", err)
+        return
+    end
+
+    ngx.say("set result: ", ok) -- 页面输出结果
+    -- put it into the connection pool of size 100,
+    -- with 10 seconds max idle time
+    local ok, err = red:set_keepalive(10000, 100) --将连接放入连接池,100个连接，最长10秒的闲置时间
+    if not ok then --判断放池结果
+        ngx.say("failed to set keepalive: ", err)
+        return
+    end
+    -- 如果不放池，用完就关闭的话，用下面的写法
+    -- or just close the connection right away:
+    -- local ok, err = red:close()
+    -- if not ok then
+    --     ngx.say("failed to close: ", err)
+    --     return
+    -- end
+```
+
+如果用过java，c#等面向对象的语言，就会觉得这么写太。。。。了，必须重构啊，暴露太多无关细节了，导致代码中有大量重复代码了。
+
+同样的内容，使用我封装后的代码。
+
+```lua
+    -- 依赖库
+    local redis = require "resty.redis-util"
+    -- 初始化
+    local red = redis:new();
+    -- 插入键值
+    local ok,err = red:set("dog","an animal")
+    -- 判断结果
+    if not ok then
+      ngx.say("failed to set dog:",err)
+      return
+    end
+    -- 页面打印结果
+    ngx.say("set result: ", ok) -- 页面输出结果
+```
+
+## 注意事项
+
+### 默认值
+
+```lua
+local red = redis:new();
+--使用了默认值,等同于
+local red2 = redis:new({host='127.0.0.1',port=6379,db_index=0,password=nil,timeout=1000,keepalive=60000,pool_size=100});
+```
+
+- host: redis host,default: 127.0.0.1
+- port: redis port,default:6379
+- db_index: redis库(0-15)，此参数预留没有用到，默认的就是0库(建议用不同端口开不同实例或者用不同前缀，因为换库需要用select命令),default:0
+- password: redis auth 认证的密码
+- timeout: reids连接超时时间,default: 1000 (1s)
+- keepalive: redis连接池的最大闲置时间, default: 60000 (1m)
+- pool_size: redis连接池大小, default: 100
+
+### subscribe
+
+因为没有用到pub/sub，所以只是简单的实现了(un)subscribe，没有继续实现(un)psubscribe(模式订阅), 参考 [Redis 接口的二次封装（发布订阅）][linkRedis接口的二次封装（发布订阅）]
+
+```lua
+
+    local cjson = require "cjson"
+    local red = redis:new();
+
+    -- 订阅dog频道
+    local func  = red:subscribe( "dog" )
+
+    -- 判断是否成功订阅
+    if not func then
+      return nil
+    end
+
+    -- 获取值
+    local res, err = func() --func()=func(true)
+    -- 如果失败，取消订阅
+    if err then
+        func(false)
+    end
+
+    -- 如果取到结果，进行页面输出
+    if res then
+        ngx.say("1: receive: ", cjson.encode(res))
+    end
+
+    -- 再次获取
+    res, err = func()
+
+    -- 获取成功后，取消订阅 func(false)
+    if res then
+        ngx.say("2: receive: ", cjson.encode(res))
+        func(false)
+    end
+
+```
+
+### pipeline
+
+参考 [openresty/lua-resty-redis#Synopsis][]
+
+```lua
+    local cjson = require "cjson"
+    local red = redis:new();
+
+    red:init_pipeline()
+
+    red:set("cat", "Marry")
+    red:set("horse", "Bob")
+    red:get("cat")
+    red:get("horse")
+
+    local results, err = red:commit_pipeline()
+
+    if not results then
+        ngx.say("failed to commit the pipelined requests: ", err)
+        return
+    else
+        ngx.say("pipeline",cjson.encode(results))
+    end
+    -- output pipeline["OK","OK","Marry","Bob"]
+```
+
+
+### script
+
+参考 [script 压缩复杂请求][linkScript压缩复杂请求]
+
+```lua
+    local red = redis:new();
+
+    local id = 1
+    local res, err = red:eval([[
+        -- 注意在 Redis 执行脚本的时候，从 KEYS/ARGV 取出来的值类型为 string
+        local info = redis.call('get', KEYS[1])
+        info = cjson.decode(info)
+        local g_id = info.gid
+
+        local g_info = redis.call('get', g_id)
+        return g_info
+        ]], 1, id)
+
+    if not res then
+       ngx.say("failed to get the group info: ", err)
+       return
+    end
+
+    ngx.say("script",res)
+```
+
+
+## 鸣谢
+
+本工具借鉴了 [lua-resty-redis/lib/resty/redis.lua][] 和 [Redis 接口的二次封装][linkRedis接口的二次封装] 的代码
+
+## 反馈
+
+如果有问题，欢迎提 [issues][]
+
+[openresty/lua-resty-redis]: https://github.com/openresty/lua-resty-redis
+[agentzh]: https://github.com/agentzh
+[README.md]: https://github.com/openresty/lua-resty-redis/blob/master/README.markdown
+[linkScript压缩复杂请求]: https://moonbingbing.gitbooks.io/openresty-best-practices/content/redis/script.html
+[openresty/lua-resty-redis#Synopsis]: https://github.com/openresty/lua-resty-redis/blob/master/README.markdown#synopsis
+[linkRedis接口的二次封装（发布订阅）]: https://moonbingbing.gitbooks.io/openresty-best-practices/content/redis/pub_sub_package.html
+[linkRedis接口的二次封装]: https://moonbingbing.gitbooks.io/openresty-best-practices/content/redis/out_package.html
+[lua-resty-redis/lib/resty/redis.lua]: https://github.com/openresty/lua-resty-redis/blob/master/lib/resty/redis.lua
+[issues]: https://github.com/anjia0532/lua-resty-redis-util/issues/new
---
+++ lab/resty/redis-util.lua
@@ -0,0 +1,244 @@
+-- Copyright (C) Anjia (anjia0532)
+
+local redis_c = require("resty.redis")
+
+local ok, new_tab = pcall(require, "table.new")
+if not ok or type(new_tab) ~= "function" then
+    new_tab = function (narr, nrec) return {} end
+end
+
+local _M = new_tab(0, 54)
+
+_M._VERSION = '0.02'
+
+local mt = {__index = _M}
+
+-- if res is ngx.null or nil or type(res) is table and all value is ngx.null return true else false
+local function _is_null(res)
+  if type(res) == "table" then
+    for _, v in pairs(res) do
+      if v ~= ngx.null then
+        return false
+      end
+    end
+  elseif res == ngx.null or res ==nil then
+    return true
+  end
+
+  return false
+end
+
+
+-- encapsulation redis connect
+local function _connect_mod(self,redis)
+  -- set timeout
+  redis:set_timeout(self._opts.timeout)
+  
+  -- set redis host,port
+  local ok, err = redis:connect(self._opts.host, self._opts.port)
+  if not ok or err then
+    return nil, err
+  end
+  
+  -- set auth
+  if self._opts.password then
+    local times, err = redis:get_reused_times()
+    if times == 0 then
+      local ok, err = redis:auth(self._opts.password)
+      if not ok or err then
+        return nil, err
+      end
+    elseif err then
+      return nil, err
+    end
+  end
+
+  return redis, nil
+end
+
+-- put it into the connection pool of size (default 100), with max idle time (default 60s)
+local function _set_keepalive_mod(self,redis )
+  return redis:set_keepalive(self._opts.keepalive, self._opts.pool_size)
+end
+
+-- encapsulation subscribe
+function _M.subscribe( self, channel )
+
+  -- init redis
+  local redis, err = redis_c:new()
+  if not redis then
+    return nil, err
+  end
+
+  -- get connect
+  local ok, err = _connect_mod(self,redis)
+  if not ok or err then
+    return nil, err
+  end
+
+  -- sub channel
+  local res, err = redis:subscribe(channel)
+  if not res then
+      return nil, err
+  end
+
+  local function do_read_func ( do_read ),
+    if do_read == nil or do_read == true then
+        res, err = redis:read_reply()
+        if not res then
+            return nil, err
+        end
+        return res
+    end
+    
+    -- if do_read is false 
+    redis:unsubscribe(channel)
+    _set_keepalive_mod(self,redis)
+    return
+  end
+
+  return do_read_func
+end
+
+-- init pipeline,default cmds num is 4
+function _M.init_pipeline(self, n)
+  self._reqs = new_tab(n or 4, 0)
+end
+
+-- cancel pipeline
+function _M.cancel_pipeline(self)
+  self._reqs = nil
+end
+
+-- commit pipeline
+function _M.commit_pipeline(self)
+  -- get cache cmds
+  local _reqs = rawget(self, "_reqs") 
+  if not _reqs then
+    return nil, "no pipeline"
+  end
+
+  self._reqs = nil
+
+  -- init redis
+  local redis, err = redis_c:new()
+  if not redis then
+    return nil, err
+  end
+
+  -- get connect
+  local ok, err = _connect_mod(self,redis)
+  if not ok or err then
+    return nil, err
+  end
+
+  redis:init_pipeline()
+  
+   --redis command like set/get ...
+  for _, vals in ipairs(_reqs) do
+    -- vals[1] is redis cmd
+    local fun = redis[vals[1]]
+    -- get params without cmd
+    table.remove(vals , 1)
+    -- invoke redis cmd 
+    fun(redis, unpack(vals))
+  end
+
+  -- commit pipeline
+  local results, err = redis:commit_pipeline()
+  if not results or err then
+    return {}, err
+  end
+
+  -- check null
+  if _is_null(results) then
+    results = {}
+    ngx.log(ngx.WARN, "redis result is null")
+  end
+
+  -- put it into the connection pool
+  _set_keepalive_mod(self,redis)
+
+  -- if null set default value nil
+  for i,value in ipairs(results) do
+    if _is_null(value) then
+      results[i] = nil
+    end
+  end
+
+  return results, err
+end
+
+-- common method
+local function do_command(self, cmd, ...)
+  
+  -- pipeline reqs
+  local _reqs = rawget(self, "_reqs")
+  if _reqs then
+    -- append reqs
+    _reqs[#_reqs + 1] = {cmd,...}
+    return
+  end
+  
+  local redis, err = redis_c:new()
+  if not redis then
+    return nil, err
+  end
+
+  local ok, err = _connect_mod(self,redis)
+  if not ok or err then
+    return nil, err
+  end
+
+  -- exec redis cmd
+  local method = redis[cmd]
+  local result, err = method(redis, ...)
+  if not result or err then
+    return nil, err
+  end
+
+  -- check null
+  if _is_null(result) then
+    result = nil
+  end
+
+  -- put it into the connection pool
+  local ok, err = _set_keepalive_mod(self,redis)
+  if not ok or err then
+    return nil, err
+  end
+
+  return result, nil
+end
+
+-- init options
+function _M.new(self, opts)
+  opts=opts or {}
+  opts={
+      host = opts.host or '127.0.0.1',
+      port = opts.port or 6379,
+      db_index = opts.db_index or 0,
+      password = opts.password or nil,
+      timeout = opts.timeout or 1000,
+      keepalive = opts.keepalive or 60000, --60s
+      pool_size = opts.pool_size or 100,
+      _reqs = nil
+  }
+  
+  return setmetatable({_opts=opts}, mt)
+end
+
+-- dynamic cmd
+setmetatable(_M, {__index = function(self, cmd)
+    local method =
+        function (self, ...)
+            return do_command(self, cmd, ...)
+        end
+
+  -- cache the lazily generated method in our
+  -- module table
+    _M[cmd] = method
+    return method
+end})
+
+return _M
